ce08e24b256637682dbafa0d29fe06efb634a699
Felipe Sutter <lipeps61@gmail.com>
2023-11-16 18:56:46 -0300

    Aula de modal

--- navegacao/routes/index.js
+++ navegacao/routes/index.js
@@ -3,15 +3,36 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import Home from "../src/pages/Home";
 import Sobre from "../src/pages/Sobre";
 import Contato from "../src/pages/Contato";
+import CustomDrawer from "../src/components/CustomDrawer";
 
 const Drawer = createDrawerNavigator();
 
 const Router = () => {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Home" component={Home} />
-      <Drawer.Screen name="Sobre" component={Sobre} />
-      <Drawer.Screen name="Contato" component={Contato} />
+    <Drawer.Navigator
+      drawerContent={CustomDrawer}
+      screenOptions={{
+        drawerActiveBackgroundColor: "#00dae5",
+        drawerInactiveBackgroundColor: "#f1f1f1",
+        drawerActiveTintColor: "#fff",
+        drawerInactiveTintColor: "#000",
+      }}
+    >
+      <Drawer.Screen
+        name="Home"
+        component={Home}
+        options={{ title: "Início" }}
+      />
+      <Drawer.Screen
+        name="Sobre"
+        component={Sobre}
+        options={{ title: "Sobre" }}
+      />
+      <Drawer.Screen
+        name="Contato"
+        component={Contato}
+        options={{ title: "Contato" }}
+      />
     </Drawer.Navigator>
   );
 };
--- navegacao/src/pages/Sobre/index.js
+++ navegacao/src/pages/Sobre/index.js
@@ -1,9 +1,30 @@
-import { View, Text, StyleSheet, Button } from "react-native";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+  Modal,
+} from "react-native";
+import ActionModal from "../../components/ActionModal";
 
 const Sobre = () => {
+  const [modal, setModal] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Tela do Sobre</Text>
+      <Text style={styles.title}>Informações</Text>
+      <TouchableOpacity style={styles.button} onPress={() => setModal(true)}>
+        <Text style={{ color: "black" }}>Abrir</Text>
+      </TouchableOpacity>
+      <Modal visible={modal} transparent={true}>
+        <ActionModal
+          handleClose={() => setModal(false)}
+          handleAjuda={() => alert("Clicou em Ajuda")}
+          handleOutros={() => alert("Clicou em Outros")}
+        />
+      </Modal>
     </View>
   );
 };
@@ -11,8 +32,23 @@ const Sobre = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
+    backgroundColor: "#121318",
     justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  button: {
+    backgroundColor: "#fff",
+    padding: 4,
+    marginVertical: 8,
+    width: "70%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 4,
   },
 });
 
